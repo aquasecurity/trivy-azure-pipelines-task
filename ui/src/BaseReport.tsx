f86e6df6ad6c6dc507ab5e18fbc023c3b922d0cf
Liam Galvin <liam.galvin@aquasec.com>
2022-06-22 15:33:16 +0100

    use tabs for base report

--- ui/src/BaseReport.tsx
+++ ui/src/BaseReport.tsx
@@ -1,57 +1,63 @@
 import * as React from 'react';
-import {
-    countReportIssues,
-    countReportMisconfigurations,
-    countReportSecrets,
-    countReportVulnerabilities,
-    Report
-} from './trivy';
+import {countReportMisconfigurations, countReportSecrets, countReportVulnerabilities, Report} from './trivy';
 import {SecretsTable} from "./SecretsTable";
-import {ZeroData} from "azure-devops-ui/ZeroData";
 import {VulnerabilitiesTable} from "./VulnerabilitiesTable";
 import {MisconfigurationsTable} from "./MisconfigurationsTable";
+import {Tab, TabBar, TabSize} from "azure-devops-ui/Tabs";
 
 interface BaseReportProps {
     report: Report
 }
 
-export class BaseReport extends React.Component<BaseReportProps> {
+interface BaseReportState {
+    selectedTabId: string
+}
+
+
+export class BaseReport extends React.Component<BaseReportProps, BaseReportState> {
 
     public props: BaseReportProps
 
     constructor(props: BaseReportProps) {
         super(props)
         this.props = props
+        this.state = {
+            selectedTabId: "vulnerabilities"
+        }
     }
+
+    private onSelectedTabChanged = (newTabId: string) => {
+        this.setState({selectedTabId: newTabId});
+    };
 
     render() {
         return (
-            countReportIssues(this.props.report) == 0 ?
-                <ZeroData
-                    primaryText="Build passed."
-                    secondaryText={
-                        <span>No problems were found within your project.</span>
-                    }
-                    imageAltText="trivy"
-                    imagePath={"images/trivy.png"}
-                /> :
-                <div className="flex-column">
-                    { countReportVulnerabilities(this.props.report) > 0 &&
-                        <div className="flex-row">
-                            <VulnerabilitiesTable results={this.props.report.Results}/>
-                        </div>
-                    }
-                    { countReportMisconfigurations(this.props.report) > 0 &&
-                        <div className="flex-row">
-                            <MisconfigurationsTable results={this.props.report.Results}/>
-                        </div>
-                    }
-                    { countReportSecrets(this.props.report) > 0 &&
-                        <div className="flex-row">
-                            <SecretsTable results={this.props.report.Results}/>
-                        </div>
-                    }
-                </div>
+            <div className="flex-column">
+                <TabBar
+                    onSelectedTabChanged={this.onSelectedTabChanged}
+                    selectedTabId={this.state.selectedTabId}
+                    tabSize={TabSize.Tall}
+                >
+                    <Tab id="vulnerabilities" name="Vulnerabilities" key="vulnerabilities"
+                         badgeCount={countReportVulnerabilities(this.props.report)}/>
+                    <Tab id="misconfigurations" name="Misconfigurations" key="misconfigurations"
+                         badgeCount={countReportMisconfigurations(this.props.report)}/>
+                    <Tab id="secrets" name="Secrets" key="secrets"
+                         badgeCount={countReportSecrets(this.props.report)}/>
+                </TabBar>
+                {
+                    this.state.selectedTabId === "vulnerabilities" &&
+                    <VulnerabilitiesTable results={this.props.report.Results}/>
+                }
+                {
+                    this.state.selectedTabId === "misconfigurations" &&
+                    <MisconfigurationsTable results={this.props.report.Results}/>
+                }
+                {
+                    this.state.selectedTabId === "secrets" &&
+                    <SecretsTable results={this.props.report.Results}/>
+                }
+            </div>
         )
     }
 }
