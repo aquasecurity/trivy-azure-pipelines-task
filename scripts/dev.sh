cc0c4400e61182783efa58b1fa2ef7cec90f8ae4
Thiago Boche <38021019+thiago-boche@users.noreply.github.com>
2023-07-12 09:41:42 +1000

    fix deployment

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -10,4 +10,4 @@ SPLIT_VERSION=$(echo $SIMPLE_VERSION | awk -F. {'printf "\"Major\":%d,\"Minor\":
 echo "Split version: ${SPLIT_VERSION}"
 sed -i "s/VERSION_PLACEHOLDER/$SPLIT_VERSION/" trivy-task/task.json
 make package-dev
-tfx extension publish --manifest-globs vss-extension.dev.json --token "${PUBLISHER_TOKEN}" --share-with liamgalvin
+tfx extension publish --manifest-globs vss-extension.dev.json --token "${PUBLISHER_TOKEN}"
