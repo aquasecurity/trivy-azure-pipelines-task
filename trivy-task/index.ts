213b6b21910cc067289f0e253c78b7ff58faef09
simar7 <1254783+simar7@users.noreply.github.com>
2024-05-15 18:44:12 -0600

    feat: Update Trivy to version v0.51.1

--- trivy-task/index.ts
+++ trivy-task/index.ts
@@ -4,7 +4,7 @@ import * as tool from 'azure-pipelines-tool-lib';
 import {ToolRunner} from 'azure-pipelines-task-lib/toolrunner';
 import task = require('azure-pipelines-task-lib/task');
 
-const latestTrivyVersion = "v0.45.1"
+const latestTrivyVersion = "v0.51.1"
 const tmpPath = "/tmp/"
 
 async function run() {
